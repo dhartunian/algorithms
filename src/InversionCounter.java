19ab37714fe2ebdb03b8cf07db1fb321ab31d60e
David Hartunian <dhartunian@gmail.com>
2012-03-24 16:35:36 -0400

    inversion counter works

--- src/InversionCounter.java
+++ src/InversionCounter.java
@@ -11,25 +11,28 @@ public class InversionCounter {
 	
 	static int[] mylist;
 	
-	public static int countInversions(int[] mysublist) {
+	public static long countInversions(int[] mysublist) {
 		if (mysublist.length == 1) return 0;
 		else {
 			int half = mysublist.length / 2;
 			int[] leftlist =  Arrays.copyOfRange(mysublist, 0, half);
 			int[] rightlist = Arrays.copyOfRange(mysublist, half, mysublist.length);
-			int left = countInversions(leftlist);
-			int right = countInversions(rightlist);
-			int split = countSplitInversions(leftlist, rightlist);
-			return left + right + split;
+			long left = countInversions(leftlist);
+			long right = countInversions(rightlist);
+			long split = countSplitInversions(leftlist, rightlist);
+			assert left >= 0;
+			assert right >= 0;
+			assert split >= 0;
+			return (left + right + split);
 		}
 	}
 	
-	public static int countSplitInversions(int[] leftlist, int[] rightlist) {
+	public static long countSplitInversions(int[] leftlist, int[] rightlist) {
 		Arrays.sort(leftlist);
 		Arrays.sort(rightlist);
 		int i = 0;
 		int j = 0;
-		int inversions = 0;
+		long inversions = 0;
 		while (i < leftlist.length && j < rightlist.length) {
 			if (rightlist[j] < leftlist[i]) {
 				inversions = inversions + leftlist.length - i;
@@ -51,11 +54,12 @@ public class InversionCounter {
 //		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 		try {
 			BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream("IntegerArray.txt")));
-			mylist = new int[100000];
+			int num_integers = 100000;
+			mylist = new int[num_integers];
 			try {
 				String line = r.readLine();
 				int i = 0;
-				while (line != null && !line.equals("")) {
+				while (line != null && !line.equals("") && i < num_integers) {
 					mylist[i] = Integer.parseInt(line);
 					i++;
 					line = r.readLine();				
@@ -63,7 +67,9 @@ public class InversionCounter {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			System.out.println(countInversions(mylist));
+			long inversions = countInversions(mylist);
+			assert inversions >= 0;
+			System.out.println(inversions);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
